685110da7cd8a06e49048ef49c1c17382a6ac27e
Hamish Redpath <49421605+hamishRedpath@users.noreply.github.com>
2022-07-18 11:15:28 +1000

    Created scene main and player controller

---
+++ GobbyJam_ProjectFiles/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Object References")]
+    [SerializeField] private Rigidbody playerRB;
+    
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
