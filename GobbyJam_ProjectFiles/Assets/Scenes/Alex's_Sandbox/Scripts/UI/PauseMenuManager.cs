93db756123362cd6b58274cb0262b2e838f96677
AlexMartinss <92001686+AlexMartinss@users.noreply.github.com>
2022-07-19 14:59:42 +1000

    :)

--- GobbyJam_ProjectFiles/Assets/Scenes/Alex's_Sandbox/Scripts/UI/PauseMenuManager.cs
+++ GobbyJam_ProjectFiles/Assets/Scenes/Alex's_Sandbox/Scripts/UI/PauseMenuManager.cs
@@ -4,13 +4,41 @@ using UnityEngine;
 
 public class PauseMenuManager : MonoBehaviour
 {
+    public GameObject PauseMenu;
     public GameObject OptionsMenu;
     public GameObject ControlsMenu;
     public GameObject ExitMenu;
 
+    public void ResumeGame()
+    {
+        PauseMenu.SetActive(false);
+    }
     public void SelectOptions()
     {
         OptionsMenu.SetActive(true);
         ControlsMenu.SetActive(false);
+    }
+
+    public void SelectControls()
+    {
+        ControlsMenu.SetActive(true);
+        OptionsMenu.SetActive(false);
+    }
+
+    public void SelectQuitScreen()
+    {
+        ControlsMenu.SetActive(false);
+        OptionsMenu.SetActive(false);
+        ExitMenu.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void DontQuitGame()
+    {
+        ExitMenu.SetActive(false);
     }
 }
