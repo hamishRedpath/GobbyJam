a2fb4fbd0d6f5f9e3dbc8271914f073d69f4cfaa
Hamish Redpath <49421605+hamishRedpath@users.noreply.github.com>
2022-07-19 11:50:15 +1000

    fuck

--- GobbyJam_ProjectFiles/Assets/Scripts/CameraController.cs
+++ GobbyJam_ProjectFiles/Assets/Scripts/CameraController.cs
@@ -6,21 +6,15 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     PlayerInput input;
-    [SerializeField] private float camSpeed;
-    private Vector2 xInput;
-    [SerializeField] private GameObject pivot;
 
     void Start()
     {
         input = PlayerController.input;
-        input.Gameplay.Camera.performed += ctx => xInput = ctx.ReadValue<Vector2>();
-        input.Gameplay.Camera.canceled += ctx => xInput = Vector2.zero;
+        
     }
 
     void Update()
     {
-        pivot.transform.Rotate(0, xInput.x * camSpeed, 0);
-        Debug.Log(xInput.x);
     }
  
 }
--- GobbyJam_ProjectFiles/Assets/Scripts/PlayerController.cs
+++ GobbyJam_ProjectFiles/Assets/Scripts/PlayerController.cs
@@ -8,16 +8,23 @@ public class PlayerController : MonoBehaviour
      public static PlayerInput input;
     [Header("Object References")]
     [SerializeField] private Rigidbody playerRB;
+    [SerializeField] private GameObject pivot;
+    [SerializeField] private Transform camTransform;
 
     [Header("Movement Variables")]
     [SerializeField] private int playerSpeed;
     private Vector2 playerVelocity;
+    private Vector2 xInput;
+    [SerializeField] private float camSpeed;
 
     private void Awake()
     {
         input = new PlayerInput();
         input.Gameplay.Movement.performed += ctx => playerVelocity = ctx.ReadValue<Vector2>();
         input.Gameplay.Movement.canceled += ctx => playerVelocity = Vector2.zero;
+
+        input.Gameplay.Camera.performed += ctx => xInput = ctx.ReadValue<Vector2>();
+        input.Gameplay.Camera.canceled += ctx => xInput = Vector2.zero;
     }
 
     void FixedUpdate()
@@ -27,7 +34,14 @@ public class PlayerController : MonoBehaviour
 
     private void Movement()
     {
-        playerRB.velocity = new Vector3 (playerVelocity.x, 0f, playerVelocity.y).normalized * playerSpeed * Time.fixedDeltaTime;
+        transform.Rotate(0, xInput.x * camSpeed, 0);
+        pivot.transform.Rotate(0, xInput.x * camSpeed, 0);
+        pivot.transform.position = playerRB.gameObject.transform.position;
+        print(playerVelocity);
+        Vector3 direction = new Vector3(playerVelocity.x, 0, playerVelocity.y);
+       // playerRB.MovePosition(playerRB.position + transform.TransformDirection(moveDirection) * Time.fixedDeltaTime * playerSpeed);
+       playerRB.velocity = transform.TransformDirection(direction) * Time.fixedDeltaTime * playerSpeed;
+
     }
 
     private void OnEnable()
